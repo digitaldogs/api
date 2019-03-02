9641fb88650e6602bb836480c49b889545ae140b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-02 19:21:02 +1000

    Add controller

---
+++ dd.api/dd.api/Controllers/MembershipController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dd.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dd.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MembershipController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public async Task<Member> Get(Member member)
+        {
+            return await Task.FromResult(new Member());
+        }
+        
+        // POST api/values
+        [HttpPost]
+        public async Task<Int32> Post(Member member)
+        {
+            return await Task.FromResult(0);
+        }
+    }
+}
