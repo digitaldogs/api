6175520e9b262467f31e5ce46b70acdae0c86df2
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-03-15 13:24:12 +1000

    Update

---
+++ dd.api/dd.models/Member.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dd.models
+{
+    public class Member
+    {
+        public String FirstName { get; set; }
+        
+        public String LastName { get; set; }
+    }
+}
